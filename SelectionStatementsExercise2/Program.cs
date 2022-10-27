20baa42652b11e3667da9658a8266279d5e98429
travisbennett03 <tbennett1996@yahoo.com>
2022-10-27 13:33:12 -0500

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is a hard subject!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is a tough subject!");
+                    break;
+                case "English":
+                    Console.WriteLine("English is a fun subject!");
+                    break;
+                case "German":
+                    Console.WriteLine("German is a great subject");
+                    break;
+                case "Gym":
+                    Console.WriteLine("Gym is a fun subject");
+                    break;
+                default:
+                    Console.WriteLine($"Oh wow! I havent taken that subject before. {subject} sounds fun!");
+                    break; 
+            }
         }
     }
 }
